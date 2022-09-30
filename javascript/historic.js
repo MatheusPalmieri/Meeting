6496342133abd8d4ac2c9819179014f777d4ba9c
FG / Matheus André Palmieri <matheus.palmieri@fgempreendimentos.com.br>
2022-09-30 15:17:35 -0300

    Update 30/09

---
+++ javascript/historic.js
@@ -0,0 +1,13 @@
+var meetingRecent = {
+    'MeetingHistoric': {
+        'name': 'TI',
+        'link': 'https://meet.jit.si/undefinedBquBRLooBa17122382022TI'
+    }
+}
+
+// localStorage.setItem('meeting', meetingRecent.id)
+// localStorage.setItem('link', meetingRecent.link)
+// console.log(localStorage.getItem('meeting'))
+// console.log(localStorage.getItem('link'))
+// console.log(meetingRecent)
+
