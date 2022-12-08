ff7cfef40a0a83fe8956b0d5c044686165cbc397
FG / Matheus André Palmieri <matheus.palmieri@fgempreendimentos.com.br>
2022-12-08 17:27:58 -0300

    Start Styled

--- javascript/recent.js
+++ javascript/recent.js
@@ -7,9 +7,9 @@ allMeetings.map((meeting) => {
   let tr = document.createElement("tr");
 
   tr.innerHTML = `
-        <td>${meeting.name}</td>
-        <td>${meeting.date}</td>
-        <td><a href="${meeting.link}" target="_blank">Open</a></td>
+        <td class="table-name">${meeting.name}</td>
+        <td class="table-date">${meeting.date}</td>
+        <td class="table-link"><a href="${meeting.link}" target="_blank">Open</a></td>
     `;
 
   table.appendChild(tr);
--- javascript/tabNavigation.js
+++ javascript/tabNavigation.js
@@ -1,14 +1,21 @@
 const nav = document.querySelectorAll("input[name='tab']");
-const contentGenerator = document.getElementById("generator");
-const contentRecent = document.getElementById("recent");
 
 const show = (tab) => {
+  const contentGenerator = document.getElementById("generator");
+  const contentRecent = document.getElementById("recent");
+  const labelCreate = document.querySelector("label[for='tabCreate']");
+  const labelRecent = document.querySelector("label[for='tabRecent']");
+
   if (tab === "tabCreate") {
     contentRecent.style.display = "none";
+    labelRecent.style.borderColor = "#fff";
     contentGenerator.style.display = "block";
+    labelCreate.style.borderColor = "#0982f3";
   } else if (tab === "tabRecent") {
     contentGenerator.style.display = "none";
+    labelCreate.style.borderColor = "#fff";
     contentRecent.style.display = "block";
+    labelRecent.style.borderColor = "#0982f3";
   }
 };
 
