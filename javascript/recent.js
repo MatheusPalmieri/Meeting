e71b04b5ee663081d5b78b36c1ddd699355bcdd7
FG / Matheus André Palmieri <matheus.palmieri@fgempreendimentos.com.br>
2022-12-09 16:58:28 -0300

    Recents Update

--- javascript/recent.js
+++ javascript/recent.js
@@ -3,14 +3,30 @@
 const table = document.getElementById("tbodyRecent");
 const allMeetings = JSON.parse(localStorage.getItem("meetingStorage"));
 
-allMeetings.map((meeting) => {
-  let tr = document.createElement("tr");
+const updateMeetings = (allMeetings) => {
+  const orderMeetings = allMeetings.reverse();
 
-  tr.innerHTML = `
-        <td class="table-name">${meeting.name}</td>
-        <td class="table-date">${meeting.date}</td>
-        <td class="table-link"><a href="${meeting.link}" target="_blank">Open</a></td>
-    `;
+  if (orderMeetings.length > 5) {
+    orderMeetings.pop();
+    localStorage.setItem("meetingStorage", JSON.stringify(orderMeetings));
+    return orderMeetings;
+  } else {
+    return orderMeetings;
+  }
+};
 
-  table.appendChild(tr);
-});
+if (allMeetings) {
+  const orderMeetings = updateMeetings(allMeetings);
+
+  orderMeetings.map((meeting) => {
+    let tr = document.createElement("tr");
+
+    tr.innerHTML = `
+          <td class="table-name">${meeting.name}</td>
+          <td class="table-date">${meeting.date}</td>
+          <td class="table-link"><a href="${meeting.link}" target="_blank">Open</a></td>
+      `;
+
+    table.appendChild(tr);
+  });
+}
